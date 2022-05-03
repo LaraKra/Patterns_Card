8dd21edf1af0a0e60e9949cb1c17d2ceac6bf3a3
LaraKra <konlor@rambler.ru>
2022-05-03 21:22:14 +0300

    add test dateMeeting

--- src/test/java/ru/netology/test/CardDelivPattern.java
+++ src/test/java/ru/netology/test/CardDelivPattern.java
@@ -35,6 +35,9 @@ public class CardDelivPattern {
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE); // предварительная чистка поля с датой по умолчанию
         $("[data-test-id='date'] input").setValue(DataGenerator.dateMeeting(6));
         $(".button").click();
+        $(withText("Необходимо подтверждение")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification_status_error .button").click();
+        $(".notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.dateMeeting(6)));
 
     }
 }
