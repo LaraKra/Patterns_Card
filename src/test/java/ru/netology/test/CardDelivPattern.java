db45c352589e1a3e4b7dc5dd7a9068b0f2edd6aa
LaraKra <konlor@rambler.ru>
2022-05-03 20:16:01 +0300

    add test date

--- src/test/java/ru/netology/test/CardDelivPattern.java
+++ src/test/java/ru/netology/test/CardDelivPattern.java
@@ -1,12 +1,9 @@
 package ru.netology.test;
 
-import com.github.javafaker.Address;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
-
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -20,5 +17,6 @@ public class CardDelivPattern {
     @Test
     public void shouldSendCorrectForm() {
         $(".input__control[type='text'][placeholder='Город']").setValue(DataGenerator.city());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE); // предварительная чистка поля с датой по умолчанию
     }
 }
